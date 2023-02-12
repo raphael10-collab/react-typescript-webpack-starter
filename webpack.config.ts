a354d9d4c502094d09ae5bec3ae8d676bef1af51
raphy <raphael.stonehorse@gmail.com>
2023-02-12 14:55:58 +0100

    commit

---
+++ webpack.config.ts
@@ -0,0 +1,49 @@
+import * as webpack from 'webpack'
+import * as webpackDevServer from 'webpack-dev-server'
+
+import path from "path";
+import { Configuration } from "webpack";
+
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "build"),
+    compress: true,
+    port: 4000,
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+  ],
+};
+
+export default config;
